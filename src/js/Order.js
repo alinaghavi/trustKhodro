294b1c2d522a2f210e405f227954c8550f630a5d
Yasin Khaki <khaki_y@irtoyaco.com>
2019-05-08 16:27:10 +0430

    i had done with request porchase

--- src/js/Order.js
+++ src/js/Order.js
@@ -3,3 +3,7 @@ $('.rounded-icon').hover(function(){
     $(this).toggleClass('shadow-sm');
     $(this).toggleClass('shadow');
 });
+$('.gallery-img').click(function () {
+    var targetAddress = $(this).children().attr('src');
+    $('.request-img').attr('src', targetAddress);
+});
