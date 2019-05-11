ae130afca4965de5f04771f0d9988b9a5d54ef21
Yasin Khaki <khaki_y@irtoyaco.com>
2019-05-11 14:25:46 +0430

    add drag scroll to gallery

--- src/js/Order.js
+++ src/js/Order.js
@@ -1,4 +1,9 @@
+import './all-import.js';
 import '../scss/Order.scss';
+import './jQuery-Plugin-Drag-To-Scroll-dragScroll/dragScroll.js';
+import '../css/jquery.mCustomScrollbar.css';
+import 'malihu-custom-scrollbar-plugin';
+import '../js/jquery.mousewheel.js';
 $('.rounded-icon').hover(function(){
     $(this).toggleClass('shadow-sm');
     $(this).toggleClass('shadow');
@@ -7,3 +12,16 @@ $('.gallery-img').click(function () {
     var targetAddress = $(this).children().attr('src');
     $('.request-img').attr('src', targetAddress);
 });
+// (function($){
+//     $(window).on("load",function(){
+//         $(".gallery").mCustomScrollbar(
+//         {
+//         axis : "x",
+//         autoExpandScrollbar:true,
+// 					advanced:{autoExpandHorizontalScroll:true},
+//           mouseWheel : {axis : 'x',
+//           scrollAmount: 200 
+//         },
+//         });
+//     });
+//   })(jQuery);
