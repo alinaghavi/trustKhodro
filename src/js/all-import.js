3ee7ebd6ede8de45e6db455c8290d4a4b9ad6f96
Yasin Khaki <khaki_y@irtoyaco.com>
2019-05-11 14:25:46 +0430

    add drag scroll to gallery

--- src/js/all-import.js
+++ src/js/all-import.js
@@ -3,7 +3,4 @@ import 'bootstrap';
 import '../scss/all-import.scss';
 import '../css/animation.css';
 import '../css/fontiran.css';
-import 'malihu-custom-scrollbar-plugin';
-import './jquery.mousewheel';
-import '../css/jquery.mCustomScrollbar.css';
 const images = require.context('../img');
