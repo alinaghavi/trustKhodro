1b65f2858f8dc94b009c55ed0f671efaa5411ec0
unknown <ahmad@Khaki-Laptop.irtoyaco.com>
2019-05-19 11:46:50 +0430

    fix multi load bug

--- src/js/all-import.js
+++ src/js/all-import.js
@@ -1,9 +1,5 @@
 import 'jquery';
 import 'bootstrap';
-import '../scss/all-import.scss';
 import '../css/animation.css';
 import '../css/fontiran.css';
-import 'malihu-custom-scrollbar-plugin';
-import './jquery.mousewheel';
-import '../css/jquery.mCustomScrollbar.css';
 const images = require.context('../img');
