90d30524fc673424888311127765c4a5b770b721
naghavi.ali <naghavi.ali@gmail.com>
2019-05-20 08:53:35 +0430

    package lock fixed

--- src/js/DashboardIndex.js
+++ src/js/DashboardIndex.js
@@ -1,1 +1,23 @@
 import '../scss/DashboardIndex.scss';
+import '../css/select2.min.css';
+import '../js/select2.min';
+
+$(document).ready(function () {
+    $("#search-wrapper .btns").hover(
+        function () {
+            var src = $(this).find('img');
+            src.attr('src', src.attr('src').replace('.svg', '-hover.svg'));
+        }, function () {
+            var src = $(this).find('img');
+            src.attr('src', src.attr('src').replace('-hover.svg', '.svg'));
+        }
+    );
+
+    $("#link-wrapper a").click(function (e) {
+        e.preventDefault();
+        var target = $(this).attr('rel');
+        $("#" + target).show().siblings("div").hide();
+    });
+
+    $('.select-box').select2();
+});
