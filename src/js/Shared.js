4eb2e6418ae64ee0664706af688b6b4d522364f1
unknown <ahmad@Khaki-Laptop.irtoyaco.com>
2019-05-23 10:45:24 +0430

    maked menu on mobile size but with some bug

--- src/js/Shared.js
+++ src/js/Shared.js
@@ -1,4 +1,13 @@
 import '../scss/shared.scss';
 $('.sm-nav-header-icon').click(function(){
-    $('.nav-header').css('display' , 'flex');
+    $('.nav-header').fadeToggle("fast");
+    $('.nav-header').toggleClass("d-flex");
+});
+$(document).click(function(event){
+    var target = $(".nav-header, .sm-nav-header-icon");
+        if(target.is(event.target) && !$('.sm-nav-header-icon').is(event.target) && target.css('display') == "flex"){
+            $('.nav-header').fadeToggle("fast");
+            $('.nav-header').toggleClass("d-flex");
+            console.log($('.nav-header').css('display'));
+        }
 });
