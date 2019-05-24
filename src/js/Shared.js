2ba40faaac724fb8f099ba9e68be5ec108b7ccb3
unknown <ahmad@Khaki-Laptop.irtoyaco.com>
2019-05-25 01:23:11 +0430

    add some responsive icon

--- src/js/Shared.js
+++ src/js/Shared.js
@@ -1,13 +1,21 @@
 import '../scss/shared.scss';
 $('.sm-nav-header-icon').click(function(){
-    $('.nav-header').fadeToggle("fast");
-    $('.nav-header').toggleClass("d-flex");
+    $('.nav-header').addClass("d-flex");
+    $('.shadow-on-nav').css("display" ,"block");
+    // $(".nav-header").css("transition" , "all .5s cubic-bezier(0.4, 0.0, 0.2, 1)");
+    $('.nav-header').css("transform" , "translateX(0)");
 });
-$(document).click(function(event){
-    var target = $(".nav-header, .sm-nav-header-icon");
-        if(target.is(event.target) && !$('.sm-nav-header-icon').is(event.target) && target.css('display') == "flex"){
-            $('.nav-header').fadeToggle("fast");
-            $('.nav-header').toggleClass("d-flex");
-            console.log($('.nav-header').css('display'));
+var target = $(".nav-header, .sm-nav-header-icon");
+$(document).click(function(){
+        if(target.css('display') == "flex"){
+            // $(".nav-header").css("transition" , "all .5s cubic-bezier(0.0, 0.0, 0.2, 1)");
+            $('.shadow-on-nav').css("display" ,"none");
+            $('.nav-header').css("transform", "");
+            setTimeout(function(){
+                $('.nav-header').removeClass("d-flex");
+            },500);
         }
+});
+target.click(function(event){
+    event.stopPropagation();
 });
